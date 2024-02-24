89e1a51351640a83d8c15d54235f2ab7ddf6f8c2
Utharaanil <utharav15@gmail.com>
2024-02-24 18:47:08 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
+
+import {  Button, Stack, TextField } from '@mui/material';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [principle, setprinciple] = useState("");
+  const [rate, setRate] = useState("");
+  const [year, setyear] = useState("");
+  const [interest, setinterest] = useState(0)
+
+ 
+
+  const calculateInterest=(e)=>{
+    e.preventDefault()
+    console.log(principle, rate, year);
+
+    if(!principle||!rate||!year){
+      alert("please enter the form completely")
+    }
+    else{
+      setinterest(principle*rate*year/100)
+    }
+  }
+
+ const resetout=()=>{
+  setprinciple("")
+  setRate("")
+  setyear("")
+  setinterest(0)
+ }
+
+ 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <>
+        <div className='container'>
+          <div className='heading-text'>
+            <h3 className='title'>SIMPLE INTEREST CALCULATOR</h3>
+            <p className='paragrap'>CALCULATE YOUR SIMPLE INTEREST</p>
+
+
+          </div>
+
+          <div className='amountcard'>
+            <div className='cardtext'>
+              <h2 className='totalamt'>₹{interest}</h2>
+              <p className='total'> Total simple interest</p>
+            </div>
+
+          </div>
+          <form onSubmit={calculateInterest} >
+            <div className='text-feilds'>
+              <div className='input'>
+                <TextField value={principle||""} onChange={e => { setprinciple(e.target.value) }}
+                  className='outlined-basic' id="outlined-basic" label="Principle Amount" variant="outlined" />
+              </div>
+
+              <div className='input'>
+                <TextField value={rate||""} onChange={e => { setRate(e.target.value) }}
+                  className='outlined-basic' id="outlined-basic" label="Rate of Interest (p.a)%" variant="outlined" />
+              </div>
+
+              <div className='input'>
+                <TextField value={year||""} onChange={e => { setyear(e.target.value) }}
+                  className='outlined-basic' id="outlined-basic" label="Time period(yr)" variant="outlined" />
+              </div>
+
+            </div>
+
+            <div className='btn-group'>
+
+              <Stack direction="row" spacing={2}>
+                <Button  className='btn' type="submit"  style={{ backgroundColor: 'black' }} variant="contained">Calculate</Button>
+                <Button onClick={resetout} className='btn' variant="outlined">Reset</Button>
+              </Stack>
+
+            </div>
+          </form>
+        </div>
+
+
+      </>
+
     </div>
   );
 }
